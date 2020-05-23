61a177b9e3414029c3d8a36ba60cd390f2b402cc
yorkyoung <58722775+york-young@users.noreply.github.com>
2020-05-23 20:37:31 +0800

    这是一个关于商城的管理端项目工程

--- common/src/main/java/com/guimei/myexception/ExceptionEnum.java
+++ common/src/main/java/com/guimei/myexception/ExceptionEnum.java
@@ -8,12 +8,35 @@ package com.guimei.myexception;
  */
 public enum ExceptionEnum {
     /**
-     * 分类信息无法找到
+     * 定义枚举数据类型
      */
-    CATEGORY_NOT_FOUND,
+    CATEGORY_NOT_FOUND(404,"商品分类没找到"),
+    BRAND_NOT_FOUND(404,"品牌不存在"),
+    SPEC_GROUP_NOT_FOUND(404,"商品规格组不存在"),
+    SPEC_PARAM_NOT_FOUND(404,"商品规格参数不存在"),
+    GOODS_NOT_FOUND(404,"商品不存在"),
+    BRAND_SAVA_ERROR(500,"品牌新增失败"),
+    GOODS_SAVA_ERROR(500,"商品新增失败"),
+    UPLOAD_FILE_ERROR(500,"文件上传失败"),
+    INVALID_FILE_TYPE(400,"无效的文件类型"),
 
-    /**
-     * 品牌信息无法找到
-     */
-    BRAND_NOT_FOUND,
+    ;
+    private int code;
+    private String msg;
+
+    ExceptionEnum() {
+    }
+
+    ExceptionEnum(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
 }
